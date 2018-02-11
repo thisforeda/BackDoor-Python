582f84578554e0feb5f0f46d1f3db3cf486a7b7e
thisforeda <thisforeda@live.cn>
2018-02-11 17:28:37 +0800

    update.

--- server.py
+++ server.py
@@ -6,34 +6,40 @@ try:
 except:
     pass
 
+NORESP = b"\xff"
+T = lambda I: I("utf-8")
+E = lambda I: T(I.encode)
+D = lambda I: T(I.decode)
+
+def enter_console(C):
+    S, R = C.send, C.recv
+    while 6:
+        inputs = input("$> ")
+        if inputs == "exit":
+            break
+        if S(E(inputs)) == 0:
+            continue
+        resp = R(4096)
+        if resp != NORESP:
+            print(D(resp))
+        else:
+            print("$> !")
+
+    C.close()
+
 def server(h, p):
     sock = socket(AF_INET, SOCK_STREAM)
     sock.bind((h, int(p)))
     sock.listen(2)
-
-    NOR = b"\xff"
-    T = lambda I: I("utf-8")
-    E = lambda I: T(I.encode)
-    D = lambda I: T(I.decode)
+    
     while 6:
-        print("listen on %s:%s, waiting for connection." % (h, p))
-        cli_sock, addr = sock.accept()
-        S, R = cli_sock.send, cli_sock.recv
-        print("connection from %s:%s" % addr)
-        while 6:
-            try:
-                inputs = input("$> ")
-                if inputs == "exit":
-                    cli_sock.close()
-                    break
-                if S(E(inputs)):
-                    response = R(1024)
-                    if response != NOR:
-                        print(D(response))
-                        continue
-                    print("$> !")
-            except Exception:
-                break
+        try:
+            print("listen on %s:%s, waiting for connection." % (h, p))
+            C, addr = sock.accept()
+            print("connection from %s:%s" % addr)
+            enter_console(C)
+        except Exception:
+            continue
 
 if __name__ == "__main__":
     import sys
