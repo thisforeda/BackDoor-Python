62a0e947f6986f94072a695e56e902ba93c1fd76
thisforeda <thisforeda@live.cn>
2018-02-11 21:24:45 +0800

    fix XXX codec can't de/encode XXX.

--- server.py
+++ server.py
@@ -6,10 +6,16 @@ try:
 except:
     pass
 
+def fuzz_den(meth):
+    for encode in ("utf-8", "gbk", "iso8859-1"):
+        try:
+            return meth(encode)
+        except Exception:
+            continue
+
 NORESP = b"\xff"
-T = lambda I: I("utf-8")
-E = lambda I: T(I.encode)
-D = lambda I: T(I.decode)
+E = lambda I: fuzz_den(I.encode)
+D = lambda I: fuzz_den(I.decode)
 
 def enter_console(C):
     S, R = C.send, C.recv
@@ -31,14 +37,14 @@ def server(h, p):
     sock = socket(AF_INET, SOCK_STREAM)
     sock.bind((h, int(p)))
     sock.listen(2)
-    
+
     while 6:
         try:
             print("listen on %s:%s, waiting for connection." % (h, p))
             C, addr = sock.accept()
             print("connection from %s:%s" % addr)
             enter_console(C)
-        except Exception:
+        except:
             continue
 
 if __name__ == "__main__":
